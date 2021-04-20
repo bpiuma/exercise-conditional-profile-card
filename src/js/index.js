75ddc50791de4298d67dfbb72ddddbe137bb40bb
Bernardo Piuma <bpiuma.uy@gmail.com>
2021-04-20 14:19:24 +0000

    Conditional Profile Card v1.0

--- src/js/index.js
+++ src/js/index.js
@@ -28,19 +28,24 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  let nombre = variables.name != null ? variables.name : "Name";
+  let apellido = variables.lastname != null ? variables.lastname : "Last Name";
+  let rol = variables.role != null ? variables.role : "Role";
+  let ciudad = variables.city != null ? variables.city : "City";
+  let pais = variables.country != null ? variables.country : "Country";
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          <h1>${nombre} ${apellido}</h1>
+          <h2>${rol}</h2>
+          <h3>${ciudad}, ${pais}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${variables.twitter}"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin}"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
